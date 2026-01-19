5557dfdbf5c1343dd2d22cf5dd12f7e176d578fb
18maldavdiya <hanish2005b@gmail.com>
2026-01-19 18:08:09 +0530

    pattern

---
+++ lecture_5/9_pattern.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i = 0 ;i<=n; i++){
+         for(int j = 1 ;j<= n-i ; j++){
+            cout<<"  ";
+        }
+        for(int k = 1; k<=i ;k++){
+            cout<<"* ";
+        }
+        for(int k = 1; k<=i-1 ;k++){
+            cout<<"* ";
+        }
+         
+        
+        cout<<endl;
+    }
+    for(int i = n ;i>=1; i--){
+         for(int j = 1 ;j<= n-i ; j++){
+            cout<<"  ";
+        }
+        for(int k = 1; k<=i ;k++){
+            cout<<"* ";
+        }
+        for(int k = 1; k<=i-1 ;k++){
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+}
