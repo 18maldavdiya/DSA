df1105b061482daed8a48050797623fd93a29aa0
18maldavdiya <hanish2005b@gmail.com>
2026-01-31 08:25:16 +0530

    hw pattern

---
+++ Hwof_pattern.cpp/1_pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i =1;i<=n;i++){
+        for(int j =1;j<=n;j++){
+            if(i==1 || i==n || j==1 ||j==n){
+                cout<<"* \t";
+            }
+            else if(i==2 || i==n-1 || j==2 ||j==n-1){
+                cout<<"# \t";
+            }
+            else{
+                cout<<"@ \t";
+            }
+        }
+        cout<<endl;
+    }
+}
