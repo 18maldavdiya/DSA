d8b67ce98b6d63a34e8511d66df7e04ec96581c5
18maldavdiya <hanish2005b@gmail.com>
2026-02-19 07:30:05 +0530

    bubble sort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -4,15 +4,21 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
+    //taking input in bubble sort
     for(int i =0;i<n;i++){
         cin>>arr[i];
     }
+    //logic of bubble sort
     for(int i =0;i<n-1;i++){
-        for(int j=1;j<n-i-1;j++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
         }
+    }
+    //printing a sorted bubble
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
    return 0;
 }
