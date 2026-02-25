f4c177dd19a148c483ece34eee40d61634a23ab2
18maldavdiya <hanish2005b@gmail.com>
2026-02-25 18:18:31 +0530

    202

--- LeetCord/202.cpp
+++ LeetCord/202.cpp
@@ -1,6 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_set>
 using namespace std;
 
-int main(){
-    if(n==1 && n==7 &)
+class Solution {
+public:
+    int getSum(int n) {
+        int sum = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while(n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = getSum(n);
+        }
+
+        return n == 1;
+    }
+};
+
+int main() {
+    Solution obj;
+    cout << obj.isHappy(19);
+    return 0;
 }
