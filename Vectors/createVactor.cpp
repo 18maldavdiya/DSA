56b2bfc5446bbc3c0c6d42eee715250f19c6e476
18maldavdiya <hanish2005b@gmail.com>
2026-03-11 10:37:07 +0530

    predefine funtion in vector

--- Vectors/createVactor.cpp
+++ Vectors/createVactor.cpp
@@ -4,8 +4,13 @@ using namespace std;
 int main(){
     vector<int> vec;
     cout<<"size="<<vec.size()<<endl;
-    for(int i : vec){
-        cout<<i<<" ";
-    }
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.pop_back();
+    cout<<"size="<<vec.size()<<endl;
+    cout<<vec.front()<<endl;
+    cout<<vec.back()<<endl;
+    cout<<vec.at(0)<<endl;
     return 0;
 }
