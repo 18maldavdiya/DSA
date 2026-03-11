64d318af25f6ad49681935e80fd9be48bde72889
18maldavdiya <hanish2005b@gmail.com>
2026-03-11 10:16:53 +0530

    vectore

--- Vectors/createVactor.cpp
+++ Vectors/createVactor.cpp
@@ -2,9 +2,10 @@
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> vec = {1,2,3,4,5};
+    vector<int> vec;
+    cout<<"size="<<vec.size()<<endl;
     for(int i : vec){
         cout<<i<<" ";
     }
-
+    return 0;
 }
