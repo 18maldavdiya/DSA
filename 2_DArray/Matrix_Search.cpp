47c7a6761d26f46df0599a3576d8a8b5b42dff91
18maldavdiya <hanish2005b@gmail.com>
2026-03-16 18:45:17 +0530

    maxtrixSearch

--- 2_DArray/Matrix_Search.cpp
+++ 2_DArray/Matrix_Search.cpp
@@ -4,24 +4,33 @@ int main(){
  int M,N;
 	cin>>M>>N;
 	
-    int arr[10][10];
+    int arr[30][30];
     for(int i = 0;i<M;i++){
         for(int j=0;j<N;j++){
             cin>>arr[i][j];
         }
     }
 int x;
-cin<<x;
+cin>>x;
+int flage =0;
 for(int i = 0 ; i < M ; i++)
 {
     for(int j = 0 ; j < N ;j++ )
     {
         if(x==arr[i][j])
         {
-           return 1;
+           flage == 1;
         }
     }
     cout<<endl;
+}
+if(flage == 1)
+{
+    cout<<"1"<<endl;
+}
+else
+{
+    cout<<"0"<<endl;
 }
 return 0;
 }
