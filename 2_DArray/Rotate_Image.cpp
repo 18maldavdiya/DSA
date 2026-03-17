6c6180cef8909299d1ec2bb3a9fd6e46ed550d59
18maldavdiya <hanish2005b@gmail.com>
2026-03-17 10:32:42 +0530

    rotating image

--- 2_DArray/Rotate_Image.cpp
+++ 2_DArray/Rotate_Image.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
-    int N;
-    cin>>N;
+int main() {
+	int n ;
+	cin>>n;
+	vector<vector<int>> arr(n,vector<int>(n));
+    for(int i = 0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+	for(int i = n-1 ; i>=0;i--)
+     {   
+       for(int j = 0 ;j<n ;j++)
+    	{   
+            cout<<arr[j][i]<<" ";
+         }
+		 cout<<endl;
+     }
+
+ 
+	return 0;
 }
