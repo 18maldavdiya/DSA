3416d26590b8b1870ff419e6922ce39a03e0ba02
18maldavdiya <hanish2005b@gmail.com>
2026-03-19 17:11:55 +0530

    non repeting

--- String/Non_Repeating_Character.cpp
+++ String/Non_Repeating_Character.cpp
@@ -1,19 +1,34 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    string s;
-    cin>>s;
-    for(int i = 0;i<s.size() ;i++){
-        int count = 0;
-        for(int j = 0; j< s.size();j++){
-            if(s[i] == s[j]){
-                count++;
+    int n;
+    cin >> n;
+
+    while(n--) {
+        string s;
+        cin >> s;
+
+        int flag = 0;
+
+        for(int i = 0; i < s.size(); i++){
+            int count = 0;
+
+            for(int j = 0; j < s.size(); j++){
+                if(s[i] == s[j]){
+                    count++;
+                }
+            }
+
+            if(count == 1){
+                cout << s[i] << endl;
+                flag = 1;
+                break;
             }
         }
-        if(count == 1){
-            cout<<s[i]<<endl;
-            break;
+
+        if(flag == 0){
+            cout << -1 << endl;
         }
     }
-
 }
