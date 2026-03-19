c3089f2b409c3bd793b7ed1039348ce2df13ddce
18maldavdiya <hanish2005b@gmail.com>
2026-03-19 18:09:22 +0530

    canyou read this from heakerblock

--- String/Can_You_Read_This.cpp
+++ String/Can_You_Read_This.cpp
@@ -1,42 +1,16 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
-// int main(){
-//     string s = "ILoveIndia";
-//     string s2 = " ";
-
-//     for(int i =0;i<s.length();i++){
-//         if(isupper(s[i])){
-//             s2.push_back(s[i]);
-//         }
-//     }
-//     cout<<s2;
-// }
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
-string s ="aaabbdccsaa";
-string ans;
-for(int i = 0 ;i<s.size();i++)
-{
-    int count =1 ;
-    for(int j = i+1 ;j<s.size();j++)
-    {
-        if(s[i]==s[j])
-        {
-            count++;
+    string s = "ILoveIndia";
+
+    for(int i =0;i<s.length();i++){
+        if(isupper(s[i])){
+            cout<<endl<<s[i];
         }
-        else
-        {
-            break;
+        else{
+            cout<<s[i];
         }
     }
-    ans.push_back(s[i]);
-    ans.push_back(count);
-
-    i=count;
-}
-cout<<ans;
-
-return 0;
+    cout<<s2;
 }
