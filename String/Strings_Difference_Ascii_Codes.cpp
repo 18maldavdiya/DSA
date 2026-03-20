44a3d49cb1abb96e2ca565cdb1c547688f6f7540
18maldavdiya <hanish2005b@gmail.com>
2026-03-20 10:29:34 +0530

    Ascii value dif

---
+++ String/Strings_Difference_Ascii_Codes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    for(int i =0;i<s.size()-1;i++){
+        int dif = s[i+1] - s[i];
+        cout<<s[i]<<dif<<"";
+    }
+     cout << s[s.size() - 1];
+}
