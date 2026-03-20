2f102a4f432f3bb74c8e280ea5cde54e09c79414
18maldavdiya <hanish2005b@gmail.com>
2026-03-20 17:29:29 +0530

    is palindrom

--- String/Strings_isPalindrome.cpp
+++ String/Strings_isPalindrome.cpp
@@ -1,24 +1,21 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
+
 int main(){
     string s;
-    cin>>s;
-    int left = 0;
-    int right = s.size() - 1;
-    int flag =0;
-    while(left<right){
-        if(s[left] ==s[right]){
-            flag = 1;
-            break;  
-        }
-        left++;
-        right--;
-    }
-    if(flag == 1){
-        cout<<"true"<<endl;
+    cin >> s;
+
+    string original = s;  // original store
+
+    reverse(s.begin(), s.end());  // reverse
+
+    if(s == original){
+        cout << "true" << endl;
     }
     else{
-        cout<<"false"<<endl;
+        cout << "false" << endl;
     }
 
+    return 0;
 }
