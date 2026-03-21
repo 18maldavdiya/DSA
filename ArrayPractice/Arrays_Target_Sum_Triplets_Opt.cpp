7d5f8faf1ae2de90d92c2c3dca27eb909d328e85
18maldavdiya <hanish2005b@gmail.com>
2026-03-21 16:15:34 +0530

    Arrays target optimise

--- ArrayPractice/Arrays_Target_Sum_Triplets_Opt.cpp
+++ ArrayPractice/Arrays_Target_Sum_Triplets_Opt.cpp
@@ -1,38 +1,52 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+
 int main(){
     int n;
     cin>>n;
     int arr[n];
-    int left = 0;
-    int right = n-1;
+
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+
     int target;
     cin>>target;
+
     sort(arr, arr + n);
+
     for(int i=0;i<n-2;i++){
         if(i>0 && arr[i] == arr[i-1]){
             continue;
         }
-        left = i+1;
-        right = n-1;
-       while(left<right){
-        int sum = arr[i] + arr[left] +arr[right];
-        if(sum ==target){
-            cout<<arr[i]<<" ,"<<arr[left]<< " and "<<arr[right]<<endl;
-            left++;
-            right--;
-        }
-        else if(sum<target){
-            left++;
-        }
-        else{
-            right--;
+
+        int left = i+1;
+        int right = n-1;
+
+        while(left < right){
+            int sum = arr[i] + arr[left] + arr[right];
+
+            if(sum == target){
+                cout<<arr[i]<<" , "<<arr[left]<<" and "<<arr[right]<<endl;
+
+                while(left < right && arr[left] == arr[left+1]){
+                    left++;
+                }
+
+                while(left < right && arr[right] == arr[right-1]){
+                    right--;
+                }
+
+                left++;
+                right--;
+            }
+            else if(sum < target){
+                left++;
+            }
+            else{
+                right--;
+            }
         }
     }
-}
-
 }
