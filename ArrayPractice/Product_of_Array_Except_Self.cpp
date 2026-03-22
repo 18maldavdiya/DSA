1e2d9a6a5c225ab28a558a1c25729ed0a77b525c
18maldavdiya <hanish2005b@gmail.com>
2026-03-22 16:03:52 +0530

    product

--- ArrayPractice/Product_of_Array_Except_Self.cpp
+++ ArrayPractice/Product_of_Array_Except_Self.cpp
@@ -3,10 +3,22 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    long long arr[n];
     for(int i =0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i = n;i>0;i++){
-        
+    long long output[n];
+    for(int i =0;i<n;i++){
+        long long product = 1;
+        for(int j=0;j<n;j++){
+            if(i != j){
+                product *= arr[j];
+            }
+        }
+        output[i] = product;
     }
+    for(int i =0;i<n;i++){
+        cout<<output[i]<<" ";
+    }
+
 }
