d68162537b62584f35a1c7b167190bedcdf36eef
18maldavdiya <hanish2005b@gmail.com>
2026-03-24 20:40:49 +0530

    reverse

---
+++ ArrayPractice/reversAnarray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int l, r;
+    cin >> l >> r;
+
+    while(l < r) {
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+
+        l++;
+        r--;
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
