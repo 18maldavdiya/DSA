b9b63fc28c5152da59e8a4274d5f9638639af35d
18maldavdiya <hanish2005b@gmail.com>
2026-03-26 21:19:38 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 using namespace std;
-int digit(string nums1, string  nums2)
+int digit(string nums1)
 {
-
+    int sum= 0 ;
+    for(int i = 0 ; i< nums1.size();i++)
+    {
+        sum = sum*10 + nums1[i];
+    }
+    cout<<sum<<endl;
+  return sum ;
 }
-int main ()
+
+int main()
 {
 string num1 = "11";
-string num2 = "22 ";
-
-
+string num2 = "22";
+int n =digit(num1);
+int m =digit(num2);
+int a= m*n;
+cout<<a<<endl;
+string ans;
+ans.push_back(a);
+cout<<ans; 
 }
